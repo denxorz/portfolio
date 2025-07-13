378b50da0e74dedaa53a8de409672530689e2bbe
Dennis Geldhof <dennis@geldhof.eu>
2025-07-13 23:11:41 +0200

    A few updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="fill-height" max-width="900">
+  <v-container class="fill-height" max-width="1440px">
     <div>
       <v-img
         class="mb-4"
@@ -21,14 +21,14 @@
 
         <v-row>
           <template v-for="(project,imgIdx) in projects" :key="imgIdx">
-            <v-col v-if="!project.children" :cols="project.cols">
-              <v-card density="compact" height="100%" :max-height="project.max === true ? '42vh' : '20vh'">
+            <v-col v-if="!project.children" :cols="project.cols ?? 3">
+              <v-card density="compact" height="100%" :max-height="project.spanRows === true ? '52vh' : '25vh'">
                 <v-card-title>{{ project.title }}</v-card-title>
                 <v-card-subtitle>{{ project.subtitle }}</v-card-subtitle>
                 <v-img
                   class="mt-2"
                   cover
-                  height="100%"
+                  height="90%"
                   :src="project.image ? project.image : `https://picsum.photos/500/300?image=${project.cols * 20}`"
                 />
               </v-card>
@@ -36,14 +36,14 @@
 
             <v-col v-if="project.children" class="d-flex flex-column" :cols="project?.cols ?? 6">
               <v-row>
-                <v-col v-for="(child,childIdx) in project.children" :key="childIdx" cols="12">
-                  <v-card density="compact" height="100%" max-height="20vh">
+                <v-col v-for="(child, childIdx) in project.children" :key="childIdx" :cols="child.cols ?? 6">
+                  <v-card density="compact" height="100%" max-height="25vh">
                     <v-card-title> {{ child.title }} </v-card-title>
                     <v-card-subtitle> {{ child.subtitle }} </v-card-subtitle>
                     <v-img
                       class="mt-2"
                       cover
-                      height="100%"
+                      height="78%"
                       :src="child.image ? child.image : `https://picsum.photos/500/300?image=${child.cols + childIdx}`"
                     />
                   </v-card>
@@ -116,46 +116,59 @@
   }
 
   const projects = [
+    { title: 'ICC', cols: 6, spanRows: true, image: new URL('@/assets/projects/icc.webp', import.meta.url).href, subtitle: 'TKH Security Flexposure - FlinQ' },
     {
       cols: 6,
       children: [
-        { title: 'ICC', cols: 6, image: new URL('@/assets/projects/icc.avif', import.meta.url).href, subtitle: 'TKH Security Flexposure - FlinQ' },
-        { title: 'QPark', cols: 6, image: new URL('@/assets/projects/qpark.jpg', import.meta.url).href, subtitle: 'TKH Security Flexposure - FlinQ' },
+        { title: 'QPark', image: new URL('@/assets/projects/qpark.jpg', import.meta.url).href, subtitle: 'TKH Security Flexposure - FlinQ' },
+        { title: 'Eurojust', image: new URL('@/assets/projects/eurojust2.jpg', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
+        { title: 'Mucci Farms', image: new URL('@/assets/projects/mucci.webp', import.meta.url).href, subtitle: 'Priva - FS Performance' },
+        { title: 'VIONIC powered by INTELLO', image: new URL('@/assets/projects/metrohm.jpg', import.meta.url).href, subtitle: 'Metrohm Autolab' },
       ],
     },
-    { title: 'Intergrow Greenhouses', cols: 6, max: true, image: new URL('@/assets/projects/intergrow.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
-    { title: 'BESI', cols: 4, image: new URL('@/assets/projects/besi.jpg', import.meta.url).href, max: true, subtitle: 'Sioux' },
-    { title: 'Green Empire Farms', cols: 4, image: new URL('@/assets/projects/greenempire.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
-    { title: 'Eurojust', cols: 4, image: new URL('@/assets/projects/eurojust2.jpg', import.meta.url).href, max: true, subtitle: 'TKH Security - Flexposure FlinQ' },
-    { title: 'Mucci Farms', cols: 12, image: new URL('@/assets/projects/mucci.webp', import.meta.url).href, subtitle: 'Priva - FS Performance' },
-    { title: 'VIONIC powered by INTELLO', cols: 4, max: true, image: new URL('@/assets/projects/metrohm.jpg', import.meta.url).href, subtitle: 'Metrohm Autolab' },
-    { title: 'Nature Fresh Farms', cols: 4, max: true, image: new URL('@/assets/projects/naturefresh.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
-    { title: 'Zuidkoop - NextDelft', cols: 4, max: true, image: new URL('@/assets/projects/zuidkoop.jpg', import.meta.url).href, subtitle: 'Nurtio - Protect' },
+
+    { title: 'Plant Solutions', image: new URL('@/assets/projects/plantsolutions.jpg', import.meta.url).href, subtitle: 'Nurtio - Protect' },
+    { title: 'Landal', image: new URL('@/assets/projects/landal.webp', import.meta.url).href, subtitle: 'Supershift - Landal' },
+    { title: 'BESI', image: new URL('@/assets/projects/besi.jpg', import.meta.url).href, subtitle: 'Sioux' },
+    { title: 'Eurojust', image: new URL('@/assets/projects/eurojust2.jpg', import.meta.url).href, max: true, subtitle: 'TKH Security - Flexposure FlinQ' },
+
     {
       cols: 6,
       children: [
-        { title: 'Asfinag', cols: 6, image: new URL('@/assets/projects/asfinag.jpg', import.meta.url).href, subtitle: 'TKH Security - Siqura Network Recorder' },
-        { title: 'Plant Solutions', cols: 6, image: new URL('@/assets/projects/plantsolutions.jpg', import.meta.url).href, subtitle: 'Nurtio - Protect' },
+        { title: 'Zuidkoop - NextDelft', image: new URL('@/assets/projects/zuidkoop.jpg', import.meta.url).href, subtitle: 'Nurtio - Protect' },
+        { title: 'Asfinag', image: new URL('@/assets/projects/asfinag.jpg', import.meta.url).href, subtitle: 'TKH Security - Siqura Network Recorder' },
+        { title: 'First Floral Company', image: new URL('@/assets/projects/firstfloral.avif', import.meta.url).href, subtitle: 'One Big Green' }, // https://www.firstfloral.com.ua/  
+        { title: 'SK Roses', image: new URL('@/assets/projects/skroses.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
       ],
     },
-    { title: 'Avantium', cols: 6, max: true, image: new URL('@/assets/projects/avantium.webp', import.meta.url).href },
-    { title: 'SK Roses', cols: 4, image: new URL('@/assets/projects/skroses.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
-    { title: 'Hexo', cols: 4, image: new URL('@/assets/projects/hexo.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
-    { title: 'SEIN', cols: 4, image: new URL('@/assets/projects/sein.webp', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
-    { title: 'Arrestantencomplex Borne', cols: 4, image: new URL('@/assets/projects/borne.avif', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
-    { title: 'RATP', cols: 4, image: new URL('@/assets/projects/ratp.jpeg', import.meta.url).href, subtitle: 'TKH Security - Siqura Network Recorder' },
-    { title: 'Rabobank', cols: 4, image: new URL('@/assets/projects/rabobank.png', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
-    { title: 'Interparking', cols: 6, image: new URL('@/assets/projects/interparking.webp', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
-    { title: 'Highway Taiwan', cols: 6, image: new URL('@/assets/projects/taiwan.jpg', import.meta.url).href, subtitle: 'TKH Security - Siqura Network Recorder' },
-    { title: 'Nurtio', cols: 6 },
-    { title: 'Supershift', cols: 6 },
-    { title: 'Ambius', cols: 6 }, // https://www.ambius.nl/
-    { title: 'leaflike', cols: 6 }, // https://www.leaflike.co.uk/
-    { title: 'donkergroep', cols: 6 }, // https://donkergroep.com/nl/donker-interieur/  https://www.firstfloral.com.ua/
-    { title: 'firstfloral', cols: 6 }, // https://www.firstfloral.com.ua/  https://greenoakms.com/garden-center/
-    { title: 'greenoak', cols: 6 }, //  https://greenoakms.com/garden-center/
+    { title: 'Intergrow Greenhouses', cols: 6, spanRows: true, image: new URL('@/assets/projects/intergrow.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
+
+    { title: 'Arrestantencomplex Borne', image: new URL('@/assets/projects/borne.avif', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
+    { title: 'RATP', image: new URL('@/assets/projects/ratp.jpeg', import.meta.url).href, subtitle: 'TKH Security - Siqura Network Recorder' },
+    { title: 'Rabobank', image: new URL('@/assets/projects/rabobank.png', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
+    { title: 'Leaflike', image: new URL('@/assets/projects/leaflike.jpg', import.meta.url).href, subtitle: 'One Big Green' }, // https://www.leaflike.co.uk/
+
+    {
+      cols: 6,
+      children: [
+        { title: 'Avantium', image: new URL('@/assets/projects/avantium.webp', import.meta.url).href },
+        { title: 'Hexo', image: new URL('@/assets/projects/hexo.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
+        { title: 'donkergroep' }, // https://donkergroep.com/nl/donker-interieur/
+        { title: 'Micromedia', image: new URL('@/assets/projects/micromedia.webp', import.meta.url).href, subtitle: 'Supershift - Micromedia' },
+      ],
+    },
+    { title: 'Mission:Control', cols: 6, spanRows: true, image: new URL('@/assets/projects/missioncontrol.webp', import.meta.url).href, subtitle: 'Supershift - Mission:Control' }, // https://www.missioncontrol.io/
+
+    { title: 'Ambius UK', image: new URL('@/assets/projects/ambius.jpg', import.meta.url).href, subtitle: 'Nurtio - Protect' }, // https://www.ambius.co.uk/
+    { title: 'Nature Fresh Farms', image: new URL('@/assets/projects/naturefresh.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
+    { title: 'SEIN', image: new URL('@/assets/projects/sein.webp', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
+    { title: 'SK Roses', image: new URL('@/assets/projects/skroses.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
+
+    { title: 'Green Empire Farms', image: new URL('@/assets/projects/greenempire.jpg', import.meta.url).href, subtitle: 'Priva - FS Performance' },
+    { title: 'Interparking',  image: new URL('@/assets/projects/interparking.webp', import.meta.url).href, subtitle: 'TKH Security - Flexposure FlinQ' },
+    { title: 'Highway Taiwan',  image: new URL('@/assets/projects/taiwan.jpg', import.meta.url).href, subtitle: 'TKH Security - Siqura Network Recorder' },
     
-    { title: 'greenoak', cols: 6 }, //  https://greenoakms.com/garden-center/
+    { title: 'greenoak' }, //  https://greenoakms.com/garden-center/
   ]
 
 // https://bloomandflourishco.com/
